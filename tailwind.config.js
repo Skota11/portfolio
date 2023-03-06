ab5cb6d968e9babc26d7b2f37f02b82081b27da8
S Kota <91359399+Skota11@users.noreply.github.com>
2023-03-06 15:50:53 +0900

    first_commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+   
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors : {
+            "half_baked" : "#8bcbc2",
+            "moss_green" : "#a6d8b1",
+            "sprout" : "#c2d6a8",
+            "vanilla" : "#d1c4a8",
+            "cold_purple" : "#b1a6d8",
+            "martinique" : "#2b2e4a",
+            "osyarena_kuro" : "#333"
+        }
+      },
+    },
+    plugins: [],
+  }
