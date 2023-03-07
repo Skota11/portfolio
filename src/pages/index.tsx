27d68f276561131684a4054dc5d4a808a0ab7a0c
S Kota <91359399+Skota11@users.noreply.github.com>
2023-03-08 01:47:18 +0900

    update_link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import  Link  from "next/link";
 import { Inter } from 'next/font/google'
 
 //module
@@ -36,10 +37,10 @@ export default function Home() {
           ...springY,
         }}>
         <header className='bg-half_baked rounded-b-xl shadow-xl text-martinique mb-4'>
-          <p className='text-center py-4 text-xl'><a href="/">skota11.com</a></p>
+          <p className='text-center py-4 text-xl'><Link href="/">skota11.com</Link></p>
           <ul className='gap-x-4 flex place-content-center text-sm'>
-            <li className='ml-4'><a href="/">Top</a></li>
-            <li className=''><a href="/works">Works</a></li>
+            <li className='ml-4'><Link href="/">Top</Link></li>
+            <li className=''><Link href="/works">Works</Link></li>
             </ul>
         </header>
         </animated.div>
