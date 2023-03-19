a61f9536ebc2681a89dab46bf696b5d998e802de
S Kota <91359399+Skota11@users.noreply.github.com>
2023-03-19 17:02:54 +0900

    add_github_button

--- components/footer.tsx
+++ components/footer.tsx
@@ -22,7 +22,8 @@ import { faDiscord, faGithub, faTwitter } from '@fortawesome/free-brands-svg-ico
             <div className='w-4/5'>
               <p className='mb-4'>Skota11’s portfolio</p>
               <p className='float-right	'><Link href="/" className='mr-2'>Top</Link><Link className='mr-4' href="/works">Works</Link> &copy; Skota11</p>
-              <a href="https://twitter.com/kota_pclive"><p className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-fit'><FontAwesomeIcon icon={faTwitter} width="30px" className='inline mr-2' />Twitterをフォローする</p></a>
+              <a href="https://twitter.com/kota_pclive"><p className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-fit my-4'><FontAwesomeIcon icon={faTwitter} width="30px" className='inline mr-2' />Twitterをフォローする</p></a>
+              <a href="https://twitter.com/kota_pclive"><p className='bg-transparent hover:bg-black-500 text-black-700 font-semibold hover:text-white py-2 px-4 border border-black-500 hover:border-transparent rounded w-fit my-4'><FontAwesomeIcon icon={faGithub} width="30px" className='inline mr-2' />Githubをフォローする</p></a>
             </div>
           </footer>
           </animated.div>
