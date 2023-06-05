126d67cd1de98ff4b694fd9a0d77d1f0d070248f
S Kota <91359399+Skota11@users.noreply.github.com>
2023-06-05 23:21:11 +0900

    AddGoogleAnalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
 import '@/styles/globals.css'
+import { GA_TRACKING_ID, pageview } from '../libs/gtag';
 import type { AppProps } from 'next/app'
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、処理終了
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return <>
-  <Header></Header>
-  <Component {...pageProps} />
-  <Footer></Footer>
+    <Header></Header>
+    <Component {...pageProps} />
+    <Footer></Footer>
   </>
 }
