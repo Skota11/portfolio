a7c67dc93fbc0e6b1c46c94924c1f508767d03aa
S Kota <91359399+Skota11@users.noreply.github.com>
2023-06-12 16:02:59 +0900

    updateWorks

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -30,53 +30,59 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://avatars.githubusercontent.com/u/91359399" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
       </Head>
-        <animated.div style={{...springX,}}>
+      <animated.div style={{ ...springX, }}>
         <h1 className='text-center text-xl'>Skota11のホームページにようこそ！</h1>
-        </animated.div>
-        <main>
-          <animated.div style={{
+      </animated.div>
+      <main>
+        <animated.div style={{
           ...springX,
         }} className='my-8' id="profile">
-            <h1 className='text-center text-2xl underline underline-offset-4 mb-4'>Works</h1>
-            <div className='flex place-content-center'>
+          <h1 className='text-center text-2xl underline underline-offset-4 mb-4'>Works</h1>
+          <div className='flex place-content-center'>
             <div className='rounded-xl bg-sprout w-4/5 max-w-screen-md p-8 shadow-lg duration-300 hover:shadow-neutral-300'>
               <div className='border-l-4 pl-4 my-6'>
-              <a href="https://mi.skota11.com" target="_blank" rel="noopener noreferrer">
-                <h2>KotaSkey</h2>
-                <p>僕のラズパイで建てている、Misskeyサーバーです。</p>
+                <a href="https://mi.skota11.com" target="_blank" rel="noopener noreferrer">
+                  <h2>KotaSkey</h2>
+                  <p>僕のラズパイで建てている、Misskeyサーバーです。</p>
                 </a>
               </div>
               <div className='border-l-4 pl-4 my-6'>
-              <a href="https://github.com/Skota11/gyaku-bot" target="_blank" rel="noopener noreferrer">
-                <h2>GyakuBot</h2>
-                <p>逆翻訳をしてくれる、DiscordBotです。</p>
+                <a href="https://github.com/Skota11/gyaku-bot" target="_blank" rel="noopener noreferrer">
+                  <h2>GyakuBot</h2>
+                  <p>逆翻訳をしてくれる、DiscordBotです。</p>
                 </a>
               </div>
               <div className='border-l-4 pl-4 my-6'>
-              <a href="https://github.com/Skota11/screenlive" target="_blank" rel="noopener noreferrer">
-                <h2>ScreenLive</h2>
-                <p>あなたの画面を簡単に共有できます。</p>
+                <a href="https://github.com/Skota11/screenlive" target="_blank" rel="noopener noreferrer">
+                  <h2>ScreenLive</h2>
+                  <p>あなたの画面を簡単に共有できます。</p>
                 </a>
               </div>
               <div className='border-l-4 pl-4 my-6'>
-              <a href="https://github.com/Skota11/twi-label" target="_blank" rel="noopener noreferrer">
-                <h2>TwiLabel</h2>
-                <p>ツイートラベルを復活させます。(Twitter for xx)</p>
+                <a href="https://github.com/Skota11/twi-label" target="_blank" rel="noopener noreferrer">
+                  <h2>TwiLabel</h2>
+                  <p>ツイートラベルを復活させます。(Twitter for xx)</p>
                 </a>
               </div>
               <div className='border-l-4 pl-4 my-6'>
-              <a href="https://vc.skota11.repl.co/" target="_blank" rel="noopener noreferrer">
-                <h2>Let’s VC</h2>
-                <p>同じ名前の部屋に入るだけで、VCができる。</p>
+                <a href="https://nextyt.netlify.app" target="_blank" rel="noopener noreferrer">
+                  <h2>NextTube</h2>
+                  <p>Youtubeを快適に視聴するクライアント</p>
+                </a>
+              </div>
+              <div className='border-l-4 pl-4 my-6'>
+                <a href="https://together.tube.skota11.com" target="_blank" rel="noopener noreferrer">
+                  <h2>TogetherYoutube</h2>
+                  <p>Youtubeの動画をみんなでウォッチパーティー</p>
                 </a>
               </div>
             </div>
-            </div>
-          </animated.div>
-        </main>
+          </div>
+        </animated.div>
+      </main>
     </>
   )
 }
